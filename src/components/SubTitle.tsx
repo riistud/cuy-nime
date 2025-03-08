cb2e0131f3187c03b1340e98bb9df23c3497169f
mugni1 <asepabdulmugni22@gmail.com>
2025-03-08 08:38:31 +0700

    update

--- src/components/SubTitle.tsx
+++ src/components/SubTitle.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function SubTitle({
   title,
   children,
@@ -6,9 +8,17 @@ export default function SubTitle({
   children?: React.ReactNode;
 }) {
   return (
-    <h1 className="font-bold text-2xl flex gap-1 items-center mt-5">
-      {children}
-      {title}
-    </h1>
+    <div className="flex items-center justify-between gap-2  mt-5">
+      <h1 className="font-bold text-2xl flex gap-1 items-center">
+        {children}
+        {title}
+      </h1>
+      <Link
+        href={"/"}
+        className="text-white text-base bg-amber-500 px-3 py-1 rounded-md hover:bg-amber-600 transition-all ease-in-out duration-200 font-semibold"
+      >
+        Show All
+      </Link>
+    </div>
   );
 }
