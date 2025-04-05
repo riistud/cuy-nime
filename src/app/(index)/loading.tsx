5072719ccceda690465801008c0b69baa283d372
mugni1 <asepabdulmugni22@gmail.com>
2025-04-05 19:06:12 +0700

    move error page

--- src/app/(index)/loading.tsx
+++ src/app/(index)/loading.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function loading() {
+export default function Loading() {
   return (
     <div className="flex items-center justify-center min-h-screen w-full">
       <div className="loader w-12 md:w-20"></div>
