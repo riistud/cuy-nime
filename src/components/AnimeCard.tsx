cef160c76e22d853034402000dd3614f27630f8f
mugni1 <asepabdulmugni22@gmail.com>
2025-04-05 08:57:33 +0700

    modified anime card

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 interface Props {
   image: string;
   title: string;
@@ -5,9 +6,11 @@ interface Props {
   type: string;
   slug: string;
 }
+import { Play } from "@phosphor-icons/react";
 import Link from "next/link";
 
 export default function AnimeCard({ image, title, status, type, slug }: Props) {
+  const typeLowerCase = type.toLowerCase();
   return (
     <div className="columns-1 bg-[var(--secondary)] rounded-lg overflow-hidden hover:scale-105 transition-all ease-in-out duration-200 font-sans group">
       <div
@@ -19,22 +22,16 @@ export default function AnimeCard({ image, title, status, type, slug }: Props) {
         className=" h-52 md:h-72  overflow-hidden relative"
       >
         <Link href={"/anime/" + slug}>
-          <span className=" absolute p-1 rounded-lg bg-red-600 text-white top-3 right-3 text-xs">
+          {/* type  */}
+          <span
+            className={`absolute p-1 rounded-lg text-white top-2 right-2 text-xs ${
+              typeLowerCase == "tv" ? "bg-sky-600" : "bg-red-600"
+            }`}
+          >
             {type}
           </span>
           <div className="absolute top-0 left-0 bottom-0 flex items-center justify-center w-full h-full bg-black opacity-0 group-hover:opacity-70 transition-all ease-in-out duration-200">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="size-20"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <Play size={80} weight="fill" />
           </div>
         </Link>
       </div>
