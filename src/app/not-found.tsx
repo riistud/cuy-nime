f21f6e3fbed7916a1c1bc9701d3a2ca2401dd33b
mugni1 <asepabdulmugni22@gmail.com>
2025-04-05 09:55:43 +0700

    modified loading

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,12 +4,13 @@ import React from "react";
 export default function NotFound() {
   return (
     <section className="w-full min-h-screen items-center justify-center flex flex-col font-sans">
-      <h1 className="text-4xl text-red-500 font-bold">404</h1>
-      <h3 className="text-2xl font-semibold">Not Found</h3>
-      <Link href={"/"}>
-        <button className="my-4 text-base py-2 px-6 bg-[var(--accent)] rounded-lg font-semibold cursor-pointer">
-          Back Home
-        </button>
+      <h1 className="text-6xl text-red-500 font-extrabold">404</h1>
+      <h3 className="text-2xl font-semibold text-[var(--accent)]">NOT FOUND</h3>
+      <Link
+        href={"/"}
+        className="my-4 text-base underline rounded-lg font-semibold cursor-pointer hover:text-[var(--accent)] "
+      >
+        Back to home
       </Link>
     </section>
   );
