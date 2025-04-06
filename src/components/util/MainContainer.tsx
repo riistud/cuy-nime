affa8aebd19f90958be1056070fd126ea1c980b5
mugni1 <asepabdulmugni22@gmail.com>
2025-04-06 11:06:07 +0700

    modified main container

--- src/components/util/MainContainer.tsx
+++ src/components/util/MainContainer.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 
 export default function MainContainer({ children }: { children: ReactNode }) {
   return (
-    <section className="w-full container mx-auto flex flex-col gap-3 px-5 pt-24 md:pt-20 font-sans">
+    <section className="w-full container mx-auto flex flex-col gap-3 px-5 pt-24 md:pt-18">
       {children}
     </section>
   );
