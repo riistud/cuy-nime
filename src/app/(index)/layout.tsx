8c519a2ba36fbed57604b9337be1f79c200b7d96
mugni1 <asepabdulmugni22@gmail.com>
2025-04-08 19:07:12 +0700

    add footer

--- src/app/(index)/layout.tsx
+++ src/app/(index)/layout.tsx
@@ -1,11 +1,13 @@
 import React, { ReactNode } from "react";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function IndexLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Navbar />
       <section className="font-sans">{children}</section>
+      <Footer />
     </>
   );
 }
