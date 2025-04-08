e9ac291008c87bd42520c69cfb3a3ead2a8f4dfc
mugni1 <asepabdulmugni22@gmail.com>
2025-04-08 19:07:30 +0700

    add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <section className=" w-full  py-5 bg-secondary font-sans">
+      <footer className=" container mx-auto px-5 flex flex-col">
+        <h1 className=" w-full text-center text-foreground text-xl md:text-2xl font-extrabold mb-2">
+          KUYNIME
+        </h1>
+        <h6 className=" w-full text-center text-foreground/50 text-xs md:text-sm">
+          Copyright © 2025 KuyNime. All Rights Reserved
+        </h6>
+        <h6 className=" w-full text-center text-foreground/50 text-xs md:text-sm">
+          Notice: This website is an unofficial and illegal website. Official
+          website gojonime.com
+        </h6>
+      </footer>
+    </section>
+  );
+}
