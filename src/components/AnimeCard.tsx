bae01c8a7361e3b800c6ff94d88da3b4ae599791
mugni1 <asepabdulmugni22@gmail.com>
2025-04-10 17:32:26 +0700

    modified anime card

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -12,18 +12,16 @@ import Link from "next/link";
 export default function AnimeCard({ image, title, status, type, slug }: Props) {
   const typeLowerCase = type.toLowerCase();
   return (
-    <div className="columns-1 bg-[var(--secondary)] rounded-lg overflow-hidden hover:scale-105  transition-all ease-in-out duration-200 font-sans group shadow-md hover:shadow-lg">
-      {/* header  */}
-      <div
-        style={{
-          backgroundImage: `url(${image})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-        className=" h-52 xl:h-72  overflow-hidden relative"
-      >
+    <div className="columns-1 bg-[var(--secondary)] rounded-lg overflow-hidden font-sans group shadow-md hover:shadow-lg">
+      {/* HEADER  */}
+      <div className=" h-52 xl:h-72  overflow-hidden relative">
         <Link href={"/anime/" + slug}>
-          {/* type  */}
+          <img
+            src={image}
+            alt=""
+            className="w-full group-hover:scale-110 transition-all ease-in-out duration-200 object-cover object-center"
+          />
+          {/* TYPE TV OR MOVIE OR ONA */}
           <span
             className={`absolute p-1 rounded-lg text-white top-2 right-2 text-xs ${
               typeLowerCase == "tv" ? "bg-sky-600" : "bg-red-600"
@@ -31,13 +29,15 @@ export default function AnimeCard({ image, title, status, type, slug }: Props) {
           >
             {type}
           </span>
-          {/* play  */}
+          {/* ICON PLAY*/}
           <div className="absolute top-0 left-0 bottom-0 flex items-center justify-center w-full h-full bg-black opacity-0 group-hover:opacity-70 transition-all ease-in-out duration-200 text-white">
             <Play size={80} weight="fill" />
           </div>
         </Link>
       </div>
-      {/* body  */}
+      {/* END HEADER  */}
+
+      {/* BODY  */}
       <div className="p-2">
         <h2 className="line-clamp-1 font-semibold group-hover:text-[var(--accent)]">
           {title}
@@ -52,6 +52,7 @@ export default function AnimeCard({ image, title, status, type, slug }: Props) {
           {status}
         </p>
       </div>
+      {/* END BODY  */}
     </div>
   );
 }
